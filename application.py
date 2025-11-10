38c0c93ab53aca5cd87e643041f85e8c1c6de00b
Ubuntu <ubuntu@ip-172-31-30-161.ec2.internal>
2025-11-10 09:33:57 +0000

    files updated and model saves in artifacts folder

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import sys
+import os
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
@@ -20,7 +22,7 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData(
+        data = CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
@@ -28,18 +30,17 @@ def predict_datapoint():
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('writing_score')),
             writing_score=float(request.form.get('reading_score'))
-
         )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
 
-        predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
-    
+        pred_df = data.get_data_as_data_frame()
+
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+
+        # ✅ Format the number properly
+        final_result = f"{results[0]:.2f}%"
+
+        return render_template('home.html', results=final_result)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)  
